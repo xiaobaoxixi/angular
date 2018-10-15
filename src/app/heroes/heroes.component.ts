746c9c4d55ac8e1aca15c9364a7946c1acf66f19
liyan <yanx0167@stud.kea.dk>
2018-10-15 08:38:25 +0200

    basic, til part 3 on https://angular.io/tutorial/toh-pt2

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../heroes";
+
+@Component({
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
+})
+export class HeroesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  };
+}
